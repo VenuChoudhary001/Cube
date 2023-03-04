a92574db57061522f7a3eccdfb2d2303231b2a00
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-03-04 22:33:28 +0530

    [UPDATE]updated links

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
     <>
       <main className="hidden  md:flex items-center container px-6 h-[104px] mx-auto justify-between">
         <div className="text-base flex gap-8 items-center">
-          <Link to='/mission' className="block ">Mission</Link>
-          <Link to="/contact" className="block ">Contact</Link>
+          <Link href='/mission' className="block ">Mission</Link>
+          <Link href="/contact" className="block ">Contact</Link>
           <span className="block ">Press</span>
         </div>
-        <button className="w-[108px] h-10 rounded-full bg-black text-white">
+        <Link href={'/waitlist'} className="w-[108px] flex items-center justify-center h-10 rounded-full bg-black text-white">
           Join beta
-        </button>
+        </Link>
       </main>
       <main className=" md:hidden relative container px-6 h-[100px] flex items-center justify-between">
         <Image 
@@ -28,9 +28,9 @@ const Navbar = () => {
         alt=""
         className="absolute left-[40%]"
         />
-         <button className="w-[88px] h-8 rounded-full bg-black text-white">
+         <Link  href={'/waitlist'} className="w-[80px] flex items-center justify-center text-sm  h-8 rounded-full bg-black text-white">
           Join beta
-        </button>
+        </Link>
        
       </main>
     </>
