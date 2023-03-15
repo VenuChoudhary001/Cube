78c1c7adf84b5a990b9f630ee0e0af01913cd865
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-03-15 20:28:14 +0530

    modified styles for home page animations

--- sections/home/hero.jsx
+++ sections/home/hero.jsx
@@ -31,15 +31,15 @@ const Hero = () => {
       css: { x: 0, opacity: 1, display: "block" },
     });
 
-    t4.from(root.current.children[3], { css: { x: -1250 } }, "start+=1.2").to(
+    t4.from(root.current.children[3], { css: { x: -1250,display:"none" } }, "start+=1.2").to(
       root.current.children[3],
-      { css: { x: 0, opacity: 1 } }
+      { css: { x: 0, opacity: 1,display:"block" } }
     );
   }, []);
 
   return (
     <>
-      <section className="relative w-full h-[100%]">
+      <section className="relative w-full h-[974px]">
         <Image
           src={"/home/1.png"}
           layout="fill"
@@ -62,15 +62,15 @@ const Hero = () => {
             <img
               src="/home/3.svg"
               alt=""
-              className=" w-[238px] 2xl:w-[280px]  h-[230px] 2xl:h-[280px]  2xl:-mt-[58px] -mt-[44px] ml-[70px] 2xl:ml-[37px] -rotate-[5deg] 2xl:rotate-[9deg] "
+              className=" w-[248px] 2xl:w-[280px]  h-[240px] 2xl:h-[280px]  2xl:-mt-[45px] -mt-[25px] hidden ml-[70px] 2xl:ml-[37px] rotate-[10deg] 2xl:rotate-[1deg] "
             />
             <img
               src="/home/4.svg"
               alt=""
-              className=" w-[230px] 2xl:w-[250px]  h-[230px] 2xl:h-[280px] mt-[30px] rotate-[20deg] mr-[73px] 2xl:hidden 2xl:mr-[52px] 2xl:rotate-[15deg] "
+              className=" w-[240px] hidden 2xl:w-[250px]  h-[240px] 2xl:h-[280px] mt-[50px] 2xl:mt-[30px] rotate-[20deg] mr-[53px] 2xl:hidden 2xl:mr-[52px] 2xl:rotate-[18deg] "
             />
           </div>
-          <div className="text-7xl font-bold min-h-[350px] flex flex-col items-center justify-center text-center text-white">
+          <div className="text-7xl  font-bold min-h-[350px] flex flex-col items-center justify-center text-center text-white">
             You’ve matched!
             <div className="text-xl font-normal">
               You and Jessica have common interests.
