c6a3d622a4233c588a675918edc54eee46d2b259
VenuChoudhary001 <venu.choudhary2000@gmail.com>
2023-03-23 12:10:13 +0530

    [UPDATE]cloudinary video

--- components/videoPlayer.jsx
+++ components/videoPlayer.jsx
@@ -7,7 +7,7 @@ const VideoPlayer = () => {
     else {
       cloudinaryRef.current = window.cloudinary;
       cloudinaryRef.current.videoPlayer(videoRef.current, {
-        cloud_name: "dtwhf1fv8",
+        cloud_name: "dyn4h2fvd",
         loop: true,
         autoplayMode: "on-scroll",
       });
@@ -15,9 +15,9 @@ const VideoPlayer = () => {
   }, []);
   return (
     <video
-      className="absolute h-[60vh] top-0 left-0 w-full md:h-full object-cover  "
+      className="absolute h-full top-0 left-0 w-full md:h-full object-cover  "
       ref={videoRef}
-      data-cld-public-id="dating"
+      data-cld-public-id={"cube_video"}
       autoPlay
       loop
     />
